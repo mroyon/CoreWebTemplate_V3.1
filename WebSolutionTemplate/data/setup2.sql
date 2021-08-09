6302afe9721459865b225e466365adbe53771ee5
mahmudur rahman <mahmud@kuwaitarmy.gov.kw>
2021-08-09 23:22:37 +0200

    Update setup2.sql

--- WebSolutionTemplate/data/setup2.sql
+++ WebSolutionTemplate/data/setup2.sql
@@ -2,63 +2,58 @@ GO
 USE KAFBlackPortal
 GO
 
-
-/*
-KAF Information Center
-*/
-
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO	
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO	
-/*
-Creator : KAF
-*/
-CREATE PROCEDURE gen_doctype_Upd222
-		@DocTypeID bigint  = NULL,
-		@DocName nvarchar (150) = NULL,
-		@DocPriority bigint  = NULL,
-		@Remarks nvarchar (350) = NULL,
-		        
-        @RETURN_KEY bigint = null output,
-        @CreatedByUserName nvarchar(256)= NULL,
-        @UpdatedByUserName nvarchar(256),
-        @CreatedDate DATETIME= NULL,
-        @UpdatedDate DATETIME,
-        @IPAddress varchar(100)= NULL,
-        @TransID nvarchar(250) ,
-        @Ts bigint = NULL
-
-	AS
-	BEGIN
-		UPDATE Gen_DocType
-		SET
-			DocName = @DocName,
-			DocPriority = @DocPriority,
-			Remarks = @Remarks,
-			TransID = @TransID,
-			UpdatedByUserName = @UpdatedByUserName,
-			UpdatedDate = @UpdatedDate,
-			IPAddress = @IPAddress
-		WHERE
-			DocTypeID = @DocTypeID
-    SET @RETURN_KEY =@DocTypeID
-		    
-	END 
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO	
-
+  
+/*  
+Creator : KAF  
+*/  
+ALTER PROCEDURE owin_userpasswordresetinfo_Ins   
+  @Serial bigint  = NULL,  
+  @SessionID varchar (250) = NULL,  
+  @UserID uniqueidentifier  = NULL,  
+  @MasterUserID bigint  = NULL,  
+  @SessionToken nvarchar (550) = NULL,  
+  @UserName nvarchar (150) = NULL,  
+  @IsActive bit  = NULL,  
             
-            
+        @RETURN_KEY bigint = null output,  
+        @CreatedByUserName nvarchar(256),  
+        @UpdatedByUserName nvarchar(256)= NULL,  
+        @CreatedDate DATETIME,  
+        @UpdatedDate DATETIME= NULL,          
+        @IPAddress varchar(100)= NULL,  
+        @TransID nvarchar(250) ,  
+        @Ts bigint = NULL  
+ AS  
+ BEGIN  
 
+	UPDATE Owin_UserPasswordResetInfo SET	
+	Owin_UserPasswordResetInfo.IsActive	 = 0
+	WHERE Owin_UserPasswordResetInfo.UserID = @UserID
+	AND Owin_UserPasswordResetInfo.UpdatedByUserName IS NULL
 
-
+  INSERT INTO Owin_UserPasswordResetInfo (  
+   SessionID,  
+   UserID,  
+   MasterUserID,  
+   SessionToken,  
+   UserName,  
+   IsActive,  
+   TransID,  
+   CreatedByUserName,  
+   CreatedDate,  
+     IPAddress  
+  )  
+  VALUES (  
+   @SessionID,  
+   @UserID,  
+   @MasterUserID,  
+   @SessionToken,  
+   @UserName,  
+   @IsActive,  
+   @TransID,  
+   @CreatedByUserName,  
+   @CreatedDate,  
+   @IPAddress  
+     )  
+  SET @RETURN_KEY = SCOPE_IDENTITY()  
+ END    
