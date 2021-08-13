59082a86d498b83ae0bb75b282072a83bd3cf80b
mahmudur rahman <mahmud@kuwaitarmy.gov.kw>
2021-08-14 01:54:42 +0200

    32.4

--- WebSolutionTemplate/WebAdmin/wwwroot/lib/customjs/utilityfunctions.js
+++ WebSolutionTemplate/WebAdmin/wwwroot/lib/customjs/utilityfunctions.js
@@ -96,5 +96,16 @@ function _cusFormValidate(formID) {
 
 $(document).ready(function () {
     $('#divprogress').hide();
+    var url = window.location;
+    // for sidebar menu entirely but not cover treeview
+    $('ul.nav-sidebar a').filter(function () {
+
+        return this.href == url;
+    }).addClass('active');
+
+    // for treeview
+    $('ul.nav-treeview a').filter(function () {
+        return this.href == url;
+    }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
 });
 //$("input[required]").parent("label").addClass("required");
